d1dcc4fb15c9c9064b859614a6eca27686a9ab06
Neil Shevlin <neilshevlin.98@gmail.com>
2022-11-30 16:30:51 +0000

    Added tests for endpoint

--- tests/test_commits.py
+++ tests/test_commits.py
@@ -8,3 +8,8 @@ client = TestClient(app)
 def test_empty_commits():
     response = client.get("/v1/commits")
     assert response.status_code == 422
+
+def test_commits():
+    res = client.get("/v1/commits?username=neilshevlin&start_date=2021-01-01&end_date=2021-01-31")
+    assert res.status_code == 200
+    assert res.json() == {"username": "neilshevlin", "start_date": "2021-01-01", "end_date": "2021-01-31"}
